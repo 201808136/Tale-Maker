12b245001b1151fd1b87b5a6322481bb0203dba9
201808136 <93645241+201808136@users.noreply.github.com>
2022-01-02 21:09:17 +0000

    Update main_write.js

--- main_write.js
+++ main_write.js
@@ -3,23 +3,6 @@ $("#restart button").on("click", deleteIt);
 $("#p2").hide();
 
 
-//array das prompts para o utilizador responder
-let prompt1 = ["-I don't know figure it out.", "-Go look for them and talk to them!", "-I'm not sure how to help maybe you should try looking for them.", "-The answer is aways love, honey!", "-(...)"];
-let prompts1 = prompt1[Math.floor(Math.random() * prompt1.length)];
-let prompt2 = ["-Thank you! - <input type='text' id='firstnamerep' name='firstname' class='firstnamereplace' onkeyup='twoWay(event)'> replied.", "-I shouldn't have asked... - <input type='text' id='firstnamerep' name='firstname' class='firstnamereplace' onkeyup='twoWay(event)'> replied.", "-Ok! I'll try that thank you so much! - <input type='text' id='firstnamerep' name='firstname' class='firstnamereplace' onkeyup='twoWay(event)'> replied.", "-That helps a lot thanks! - <input type='text' id='firstnamerep' name='firstname' class='firstnamereplace' onkeyup='twoWay(event)'> replied.", "-(...) - <input type='text' id='firstnamerep' name='firstname' class='firstnamereplace' onkeyup='twoWay(event)'> replied."];
-let prompts2 = prompt2[Math.floor(Math.random() * prompt2.length)];
-let prompt3 = ["-Where the hell have you been?", "-You found me congrats!", "-Took you long enough huh?", "-Oh hi there.", "-(...)"];
-let prompts3 = prompt3[Math.floor(Math.random() * prompt3.length)];
-let prompt4 = ["-Fuck off.", "-I'm happy you're here!", "-I'm not sure how to react.", "-I hate you!", "-(...)"];
-let prompts4 = prompt4[Math.floor(Math.random() * prompt4.length)];
-/*
-    $("#refreshbutton").on("click", refresh);
-function refresh() {
-        //volta a dar uma prompt random através do math random
-    let prompt = prompts[Math.floor(Math.random()*prompts.length)];
-    $(".writingprompt1").html(prompt);
-}
-*/
 var title = $("#ftitle").val();
 var fn = $("#firstname").val();
 var sn = $("#secondname").val();
@@ -49,6 +32,14 @@ var search27 = $("#search27").val();
 var chance = " ";
 
 
+let prompt1 = ["-I don't know figure it out.", "-Go look for them and talk to them!", "-I'm not sure how to help maybe you should try looking for them.", "-The answer is aways love, honey!", "-(...)"];
+let prompts1 = prompt1[Math.floor(Math.random() * prompt1.length)];
+let prompt2 = ["-Thank you! - "+fn+" replied.", "-I shouldn't have asked... - "+fn+" replied.", "-Ok! I'll try that thank you so much! - "+fn+" replied.", "-That helps a lot thanks! - "+fn+" replied.", "-(...) - "+fn+" replied."];
+let prompts2 = prompt2[Math.floor(Math.random() * prompt2.length)];
+let prompt3 = ["-Where the hell have you been?", "-You found me congrats!", "-Took you long enough huh?", "-Oh hi there.", "-(...)"];
+let prompts3 = prompt3[Math.floor(Math.random() * prompt3.length)];
+let prompt4 = ["-Fuck off.", "-I'm happy you're here!", "-I'm not sure how to react.", "-I hate you!", "-(...)"];
+let prompts4 = prompt4[Math.floor(Math.random() * prompt4.length)];
 
 let x = Math.floor((Math.random() * 100) + 1);
 let y = Math.floor((Math.random() * 100) + 1);
