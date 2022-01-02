3087230436994037f6907ee7e40b57c3a9949af0
201808136 <93645241+201808136@users.noreply.github.com>
2022-01-02 21:29:58 +0000

    Update main_write.js

--- main_write.js
+++ main_write.js
@@ -3,6 +3,16 @@ $("#restart button").on("click", deleteIt);
 $("#p2").hide();
 
 
+//array das prompts para o utilizador responder
+
+/*
+    $("#refreshbutton").on("click", refresh);
+function refresh() {
+        //volta a dar uma prompt random através do math random
+    let prompt = prompts[Math.floor(Math.random()*prompts.length)];
+    $(".writingprompt1").html(prompt);
+}
+*/
 var title = $("#ftitle").val();
 var fn = $("#firstname").val();
 var sn = $("#secondname").val();
@@ -30,7 +40,6 @@ var search25 = $("#search25").val();
 var search26 = $("#search26").val();
 var search27 = $("#search27").val();
 var chance = " ";
-
 
 let prompt1 = ["-I don't know figure it out.", "-Go look for them and talk to them!", "-I'm not sure how to help maybe you should try looking for them.", "-The answer is aways love, honey!", "-(...)"];
 let prompts1 = prompt1[Math.floor(Math.random() * prompt1.length)];
@@ -110,7 +119,7 @@ twoWay=function(event){
 
 function saveOnePost() {
 
-  if (title == "" || search == "" || search2 == "" || search3 == "" || search4 == "" || search5 == "" || search6 == "" || search7 == "" || search8 == "" || search9 == "" || search10 == "" || search11 == "" || search12 == "" || search15 == "" || search16 == "" || search17 == "" || search18 == "" || search20 == "" || search22 == "" || search23 == "" || search24 == "" || search25 == "" || search26 == "" || search27 == ""){
+  if (title == "" || search == "" || search2 == "" || search3 == "" || search4 == "" || search5 == "" || search6 == "" ||  search9 == "" || search10 == "" || search11 == "" || search12 == "" || search15 == "" || search16 == "" || search17 == "" || search18 == "" || search20 == "" ||  search22 == "" || search23 == "" || search24 == "" || search25 == "" || search26 == "" || search27 == ""){
     alert('Please fill all the text fields. This is an order.');
   } else{
     alert('Written by you and the computer. Your story is ready.');
@@ -140,11 +149,12 @@ $("#img6").attr("src", url6);
   var i =  localStorage.length;
 var titulos = title;
    //let conteudo = '<h1>'+ title +'</h1>  <div class="img-wrapper"><img src="'+ tiitl +'" alt="" id="t'+i+'"></div> <p> Once upon a time there was a '+ search + ' named '+ fn +', from '+ search25 +'. They had really '+ search2 +' and '+ search3 +'. They were known for '+ search4 +' and everyday they would '+ search5 +'.</p> <div class="img-wrapper"><img src="'+ url1 +'" alt="" id="img1"></div> <p> But on a '+ search6 +' '+ fn +' was in '+ search7 +' and a '+ search27 +' named '+ sn +' showed up. '+ sn +' was know for '+ search8 +'. Suddenly, something unexpected happened. '+ sn +' '+ search9 +' and disappeared. They '+ search10 +'. '+ fn +' was devastated. </p> <div class="img-wrapper"><img src="'+ url2 +'" alt="" id="img2"></div>  <p>'+ fn +' didn’t know what to do so they went to '+ search11 +' looking for '+ search12 +' who gave them advice: <br> -'+ search13 +' <br> -'+ search14 +' - '+ fn +' replied.</p><div class="img-wrapper"><img src="'+ url3 +'" alt="" id="img3"></div> <p>'+ fn +' was '+ search15 +' and decided to go looking for '+ sn +'. After '+ search16 +' for '+ search17 +' hours they finally found them. '+ sn +' was '+ search18 +', and said: <br>- '+ search19 +' </p><div class="img-wrapper"><img src="'+ url4 +'" alt="" id="img4"></div> <p>Unexpectedly '+ fn +' '+ search20 +' and said: <br> - '+ search21 +'</p><div class="img-wrapper"><img src="'+ url5 +'" alt="" id="img5"></div> <p>'+ fn +' was so '+ search22 +' and decided to '+ search23 +' with '+ search24 +'. They lived '+ search26 +' ever after.</p><div class="img-wrapper"><img src="'+ url6 +'" alt="" id="img6"></div>'
-   let conteudo = '<h1><em>'+ title +'</em></h1>  <div class="img-wrapper"><img src="'+ tiitl +'" alt="" id="t'+i+'"></div> <p> Once upon a time there was a <em>'+ search + '</em> named <em>'+ fn +'</em>, from <em>'+ search25 +'</em>. They had really <em>'+ search2 +'</em> and <em>'+ search3 +'</em>. They were known for <em>'+ search4 +'</em> and everyday they would <em>'+ search5 +'</em>.</p> <div class="img-wrapper"><img src="'+ url1 +'" alt="" id="img1"></div> <p> But on a <em>'+ search6 +'</em>, <em>'+ fn +'</em> was in <em>'+ search7 +'</em> and a <em>'+ search27 +'</em> named <em>'+ sn +'</em> showed up. <em>'+ sn +'</em> was know for <em>'+ search8 +'</em>. Suddenly, something unexpected happened. <em>'+ sn +'</em> <em>'+ search9 +'</em> and <em>'+ search10 +'</em>. <em>'+ fn +'</em> was devastated. </p> <div class="img-wrapper"><img src="'+ url2 +'" alt="" id="img2"></div>  <p><em>'+ fn +'</em> didn’t know what to do so they went to <em>'+ search11 +'</em> looking for <em>'+ search12 +'</em> who gave them advice: <br> -<em>'+ prompts1 +'</em> <br> -<em>'+ prompts2 +'</em> - <em>'+ fn +'</em> replied.</p><div class="img-wrapper"><img src="'+ url3 +'" alt="" id="img3"></div> <p><em>'+ fn +'</em> was <em>'+ search15 +'</em> and decided to go looking for <em>'+ sn +'</em>. After <em>'+ search16 +'</em> for <em>'+ search17 +'</em> hours they finally found them. <em>'+ sn +'</em> was <em>'+ search18 +'</em>, and said: <br>- <em>'+ prompts3 +'</em> </p><div class="img-wrapper"><img src="'+ url4 +'" alt="" id="img4"></div> <p>Unexpectedly <em>'+ fn +'</em> <em>'+ search20 +'</em> and said: <br> - <em>'+ prompts4 +'</em></p><div class="img-wrapper"><img src="'+ url5 +'" alt="" id="img5"></div> <p><em>'+ fn +'</em> was so <em>'+ search22 +'</em> and decided to <em>'+ search23 +'</em> with <em>'+ search24 +'</em>. They lived <em>'+ search26 +'</em> ever after.</p><div class="img-wrapper"><img src="'+ url6 +'" alt="" id="img6"></div>'
+   let conteudo = '<h1><em>'+ title +'</em></h1>  <div class="img-wrapper"><img src="'+ tiitl +'" alt="" id="t'+i+'"></div> <p> Once upon a time there was a <em>'+ search + '</em> named <em>'+ fn +'</em>, from <em>'+ search25 +'</em>. They had really <em>'+ search2 +'</em> and <em>'+ search3 +'</em>. They were known for <em>'+ search4 +'</em> and everyday they would <em>'+ search5 +'</em>.</p> <div class="img-wrapper"><img src="'+ url1 +'" alt="" id="img1"></div> <p> But on a <em>'+ search6 +'</em>, <em>'+ fn +'</em> was in <em>'+ search7 +'</em> and a <em>'+ search27 +'</em> named <em>'+ sn +'</em> showed up. <em>'+ sn +'</em> was know for <em>'+ search8 +'</em>. Suddenly, something unexpected happened. <em>'+ sn +'</em> <em>'+ search9 +'</em> and <em>'+ search10 +'</em>. <em>'+ fn +'</em> was devastated. </p> <div class="img-wrapper"><img src="'+ url2 +'" alt="" id="img2"></div>  <p><em>'+ fn +'</em> didn’t know what to do so they went to <em>'+ search11 +'</em> looking for <em>'+ search12 +'</em> who gave them advice: <br> -<em>'+ prompts1 +'</em> <br> -<em>'+ prompts2 +'</em></p><div class="img-wrapper"><img src="'+ url3 +'" alt="" id="img3"></div> <p><em>'+ fn +'</em> was <em>'+ search15 +'</em> and decided to go looking for <em>'+ sn +'</em>. After <em>'+ search16 +'</em> for <em>'+ search17 +'</em> hours they finally found them. <em>'+ sn +'</em> was <em>'+ search18 +'</em>, and said: <br>- <em>'+ prompts3 +'</em> </p><div class="img-wrapper"><img src="'+ url4 +'" alt="" id="img4"></div> <p>Unexpectedly <em>'+ fn +'</em> <em>'+ search20 +'</em> and said: <br> - <em>'+ prompts4 +'</em></p><div class="img-wrapper"><img src="'+ url5 +'" alt="" id="img5"></div> <p><em>'+ fn +'</em> was so <em>'+ search22 +'</em> and decided to <em>'+ search23 +'</em> with <em>'+ search24 +'</em>. They lived <em>'+ search26 +'</em> ever after.</p><div class="img-wrapper"><img src="'+ url6 +'" alt="" id="img6"></div>'
     console.log(conteudo);
     localStorage.setItem(titulos, conteudo);
 
- 
+    var blob = new Blob([conteudo], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, titulos);
     $("#p1").hide();
     $("#p2").show();
   }
